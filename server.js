420afd3af7759b3b7fb0621e2f1b99ea5a290e30
Or Fleisher <contact@orfleisher.com>
2017-01-31 18:16:02 -0500

    Adding folder structure, Sockets setup and building folder sniffing functionality into the server

--- server.js
+++ server.js
@@ -32,6 +32,31 @@ app.set('view-engine', 'html');
 //Setup the public client folder
 app.use(express.static(__dirname + '/public'));
 
+//Node file-system
+const fs = require('fs');
+
+//Array for storing the files
+let modelList = [];
+
+//Set routine check for models every second
+setInterval(()=>{
+    //Read the model library
+    readModelDir("/public/assets/models/", modelList);
+}, 1000);
+
+//////////////////////
+//////SOCKET.IO///////
+//////////////////////
+io.on('connection', client=>{
+    //Log the user to the server console
+    console.log('User with the ID ' + client.id + ' has connected');
+
+    //The user dissconnects
+    client.on('disconnect', ()=>{
+        //Log the user that dissconnected to the server
+        console.log('User with the ID ' + client.id + ' has disconnected');
+    });
+});
 
 //////////////////////
 //////ROUTER//////////
@@ -47,6 +72,24 @@ app.get('/admin', (req, res)=>{
     res.render('admin.html');
 });
 
+//Oops, where are you looking sir?
 app.get('/*', (req, res)=>{
     res.render('404.html');
 });
+
+//////////////////////////////////////////////////////////////
+////Some server util functions - Cleanup before deploy////////
+//////////////////////////////////////////////////////////////
+
+function readModelDir(dirPath, listObject){ 
+    //Read the folder containing the models
+    fs.readdir(__dirname + dirPath, (err, files)=>{
+        if(err){
+            console.log('There was a problem reading the directory ' + err);
+        } else {
+            console.log(files);
+
+            listObject = files;
+        }
+    });
+}
